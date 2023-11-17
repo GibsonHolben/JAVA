e3b213a379fd336886ef588b412b8e04ec84b52d
Mastermind <148354429+GibsonHolben@users.noreply.github.com>
2023-11-17 08:52:37 -0500

    Added Armor.java
    Added Armor.java and ArmorDriver.java

---
+++ IsAGroupProject/src/Armor.java
@@ -0,0 +1,147 @@
+import java.text.Format;
+import java.time.LocalDate;
+
+/**
+ * Used to create a armor that is used by a clone
+ * @author Gibson Holben
+ * @version 0.0.0.1
+ */
+public class Armor 
+{
+	//*****************************
+	//Instance vars
+	//*****************************
+	
+	/**The type of armor that the curent armor is 
+	 *Examples include: flight, shock, burning, etc...
+	 * Default = blank*/
+	private String 				ArmorType = "";
+	
+	/**The date that the armor was created*/
+	private final LocalDate 	DATECREATED;
+	
+	/**Is the armor bulletproof
+	 * Default = false*/
+	private boolean 			isBulletProof = false;
+	
+
+	/**The level of protection that the armor has
+	 * Default = 3*/
+	private short 				protectionLvl = 3;
+	
+	//*****************************
+	//Constructors
+	//*****************************
+	
+	/**
+	 * <p>The default constructor</p>
+	 * <p>Sets the date created</p>
+	 */
+	Armor()
+	{
+		DATECREATED = LocalDate.now();
+	}
+	
+	/**
+	 * Overloaded constructor that takes in all the variables
+	 * @param ArmorType		The type of armor that the curent armor is
+	 * @param isBulletProof Is the armor bulletproof
+	 * @param protectionLvl The level of protection that the armor has
+	 */
+	Armor(String ArmorType, boolean isBulletProof, short protectionLvl)
+	{
+		DATECREATED 		= LocalDate.now();
+		this.ArmorType 		= ArmorType;
+		this.isBulletProof 	= isBulletProof;
+		this.protectionLvl 	= protectionLvl;		
+	}
+	
+	//*****************************
+	//Getters and Setters
+	//*****************************
+	/**
+	 * Returns armorType 
+	 * @return the armorType
+	 */
+	public String getArmorType() {
+		return ArmorType;
+	}
+
+	/**
+	 * Sets armorType
+	 * @param armorType the new armorType 
+	 */
+	public void setArmorType(String armorType) {
+		ArmorType = armorType;
+	}
+
+	/**
+	 * Returns isBulletProof 
+	 * @return the isBulletProof
+	 */
+	public boolean getisBulletProof() {
+		return isBulletProof;
+	}
+
+	/**
+	 * Sets isBulletProof
+	 * @param isBulletProof the new isBulletProof 
+	 */
+	public void setBulletProof(boolean isBulletProof) {
+		this.isBulletProof = isBulletProof;
+	}
+
+	/**
+	 * Returns protectionLvl 
+	 * @return the protectionLvl
+	 */
+	public short getProtectionLvl() {
+		return protectionLvl;
+	}
+
+	/**
+	 * Sets protectionLvl
+	 * @param protectionLvl the new protectionLvl 
+	 */
+	public void setProtectionLvl(short protectionLvl) {
+		this.protectionLvl = protectionLvl;
+	}
+
+	/**
+	 * Returns dATECREATED 
+	 * @return the dATECREATED
+	 */
+	public LocalDate getDateCreated() {
+		return DATECREATED;
+	}
+
+	
+	
+	//*****************************
+	//Methods
+	//*****************************
+	
+	/**
+	 * Prints the instance pariables to the console in an orderly fassion
+	 */
+	public void printAll()
+	{
+		System.out.println("Armor Type: " + ArmorType);
+		System.out.println("Date Created: " + DATECREATED);
+		System.out.println("Protection Level: " + protectionLvl);
+		System.out.println("Is bullet proof: " + isBulletProof);
+	}
+	
+
+	/**
+	 * Returns the instance variables in a CSV format
+	 * @return The variables in a CSV format
+	 */
+	public String toString()
+	{
+		String S1 = ArmorType + "|" + DATECREATED + "|" + protectionLvl + "|" + isBulletProof;
+		return S1;
+	}
+	
+	
+}
---
+++ IsAGroupProject/src/ArmorDriver.java
@@ -0,0 +1,49 @@
+/**
+ * Test case for armor
+ * @author Gibson Holben
+ * @version 0.0.0.1
+ */
+public class ArmorDriver
+{
+	public static void main(String[] args)
+	{
+		System.out.println("******************************A1");
+		Armor A1 = new Armor();
+		System.out.println("******************************\n");
+		
+		System.out.println("******************************A2");
+		Armor A2 = new Armor("Shock", true, (short) 100);
+		testGettersAndSetters(A2);
+		System.out.println("******************************\n");
+		
+		System.out.println("******************************A3");
+		Armor A3 = new Armor("Astral", true, (short) 1000);
+		A3.printAll();
+		System.out.println(A3.toString());
+		System.out.println("******************************\n");
+		
+				
+	}
+	
+	/**
+	 * Tests the getters and setters on a passed in armor
+	 * @param A The armor that the test is run on
+	 */
+	public static void testGettersAndSetters(Armor A)
+	{
+		System.out.println(A.getArmorType());
+		System.out.println(A.getDateCreated());
+		System.out.println(A.getProtectionLvl());
+		System.out.println(A.getisBulletProof());
+		
+		A.setArmorType("Burning");
+		A.setProtectionLvl((short) 89);
+		A.setBulletProof(false);
+		
+		System.out.println(A.getArmorType());
+		System.out.println(A.getDateCreated());
+		System.out.println(A.getProtectionLvl());
+		System.out.println(A.getisBulletProof());
+	}
+
+}
