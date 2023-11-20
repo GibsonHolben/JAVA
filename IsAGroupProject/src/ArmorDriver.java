305dbc12be9b4740f2c0733e42bf206d0678c259
Mastermind <148354429+GibsonHolben@users.noreply.github.com>
2023-11-20 09:54:42 -0500

    Testing

--- IsAGroupProject/src/ArmorDriver.java
+++ IsAGroupProject/src/ArmorDriver.java
@@ -7,6 +7,8 @@ public class ArmorDriver
 {
 	public static void main(String[] args)
 	{
+		
+		//Test github change
 		System.out.println("******************************A1");
 		Armor A1 = new Armor();
 		System.out.println("******************************\n");
