b3346bb9cc6c79c7053c4139a5509e6c3161e7a9
Mastermind <148354429+GibsonHolben@users.noreply.github.com>
2023-11-29 10:09:40 -0500

    Updates StringEdits
    Updated the String edits code

--- InputEdits/src/StringEdits.java
+++ InputEdits/src/StringEdits.java
@@ -32,6 +32,46 @@ public class StringEdits
 		//3. spaces		- " ", "       " 
 		//4. blank		- ""
 		
+		//No spaces allowed
+		//1. " " error
+		//2. "  " error
+		//3. "   Gibson Holben" remove leaing spaces (good)
+		//4. "	 Gibson Holben	" remove trailing spaces (good)
+		
+		
+		
+		//Corect way
+		args[4] = args[4].trim();
+		
+		if(args[4].isEmpty())
+		{
+			System.out.println("args[4] length: " + args[4].length());
+			System.out.println("args[4]: " + args[4] + "error");
+
+		}
+		else
+		{
+			System.out.println("args[4]: " + args[4].trim() + " (good)");
+			System.out.println("args[4] length: " + args[4].length());
+		}
+		
+	
+		
+		//Better way above ^^^^^
+		
+		//if(args[3].trim().isEmpty()): "	 Gibson Holben	" >>> "Gibson Holben" >>> if("Gibson Holben".isEmpty()) >>> if(false)
+		if(args[3].trim().isEmpty())
+		{
+			System.out.println("args[3] length: " + args[3].length());
+			System.out.println("args[3]: " + args[3] + "error");
+
+		}
+		else
+		{
+			System.out.println("args[3]: " + args[3].trim() + " good");
+			System.out.println("args[3] length: " + args[3].length());
+		}
+		
 		
 		
 		System.out.println("***********************************************************************");
@@ -53,7 +93,46 @@ public class StringEdits
 		{
 			System.out.println("Value: " + Value + " (good)");
 			System.out.println("Value length: " + Value.length());
-			System.out.println("***********************************************************************");
+		}
+		
+		
+		
+		
+		//System.out.println("***********************************************************************");
+		
+		
+		
+		
+		
+		System.out.println("***********************************************************************");
+		if(args[0].isEmpty()) //length = 0 (wrong)
+		{
+			System.out.println("args[0]: " + args[0] + "Length must be a value greater that 0");
+			System.out.println("args[0] length: " + args[0].length());
+		}
+		else
+		{
+			System.out.println("args[0]: " + args[0] + "good");
+			System.out.println("args[0] length: " + args[0].length());
+		}
+		
+		
+		
+
+		
+		
+		System.out.println("***********************************************************************");
+		
+		if(args[1].isEmpty()) //length = 1 (right)
+		{
+			System.out.println("args[1] length: " + args[1].length());
+			System.out.println("args[1]: " + args[1] + "error");
+
+		}
+		else
+		{
+			System.out.println("args[1]: " + args[1] + "good");
+			System.out.println("args[1] length: " + args[1].length());
 		}
 		
 		
@@ -61,69 +140,19 @@ public class StringEdits
 		
 		
 		
+		System.out.println("***********************************************************************");
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-//		System.out.println("***********************************************************************");
-//		if(args[0].isEmpty()) //length = 0 (wrong)
-//		{
-//			System.out.println("args[0]: " + args[0] + "Length must be a value greater that 0");
-//			System.out.println("args[0] length: " + args[0].length());
-//		}
-//		else
-//		{
-//			System.out.println("args[0]: " + args[0] + "good");
-//			System.out.println("args[0] length: " + args[0].length());
-//		}
-//		
-//		
-//		
-//		
-//		
-//		
-//		
-//		
-//		System.out.println("***********************************************************************");
-//		
-//		if(args[1].isEmpty()) //length = 1 (right)
-//		{
-//			System.out.println("args[1] length: " + args[1].length());
-//			System.out.println("args[1]: " + args[1] + "error");
-//
-//		}
-//		else
-//		{
-//			System.out.println("args[1]: " + args[1] + "good");
-//			System.out.println("args[1] length: " + args[1].length());
-//		}
-//		
-//		
-//		
-//		
-//		
-//		
-//		System.out.println("***********************************************************************");
-//		
-//		if(args[2].isEmpty()) // "a " length = 2 (right)
-//		{
-//			System.out.println("args[2] length: " + args[2].length());
-//			System.out.println("args[2]: " + args[2] + "error");
-//
-//		}
-//		else
-//		{
-//			System.out.println("args[2]: " + args[2] + " good");
-//			System.out.println("args[2] length: " + args[2].length());
-//		}
+		if(args[2].isEmpty()) // "a " length = 2 (right)
+		{
+			System.out.println("args[2] length: " + args[2].length());
+			System.out.println("args[2]: " + args[2] + "error");
+
+		}
+		else
+		{
+			System.out.println("args[2]: " + args[2] + " good");
+			System.out.println("args[2] length: " + args[2].length());
+		}
 		
 		System.out.println("Gibson");
 	}
