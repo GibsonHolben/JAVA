8f83961e403ed88963f7960aeab615664d439f77
Mastermind <148354429+GibsonHolben@users.noreply.github.com>
2023-12-13 09:39:41 -0500

    Added an example of overloading
    Created a method that is an example of a overloaded function

--- FunctionsWithMainMethod/src/StaticMethodsOverload.java
+++ FunctionsWithMainMethod/src/StaticMethodsOverload.java
@@ -7,17 +7,47 @@ public class StaticMethodsOverload
 	//overloading
 	
 	//signature
+	//signature must be unique	printAll + String 
 	public static void printAll(String Name)
 	{
-		System.out.println(Name);
+		System.out.println("Name: " + Name);
 	}
 	
 	public static void main(String[] args)
 	{
 		String Name= "Gibson";
-		printAll(Name);	//passed the mem address not he name
+		printAll(Name);	//passed the mem address not the name
 		//16	17	 9	 11	  17   19 ; temp deleted
+		
+		
+		int myAge = 16;		//primative	gets sent rather that mem address
+		printAll(16);		//printAll String reject
+							//printAll int accept
+		//23	24	48	50	24	27
 		System.out.println("stop");
+	}
+	
+	
+	
+	
+	
+	
+	
+
+	
+	
+	
+//	Will not work	printAll + String  			< 			Illigal
+//	public static void printAll(String Address)
+//	{
+//		System.out.println(Address);
+//	}
+	
+	//Will work 	printAll + int 	
+	//Overloading
+	public static void printAll(int age)
+	{
+		System.out.println("age: " + age);
 	}
 }
 	
