ee61c03966565b631bed01de794b60b58575b4b4
Mastermind <148354429+GibsonHolben@users.noreply.github.com>
2023-12-13 10:09:10 -0500

    Finished StaticMethods
    Finished the Static methods test class

--- FunctionsWithMainMethod/src/StaticMethodsOverload.java
+++ FunctionsWithMainMethod/src/StaticMethodsOverload.java
@@ -17,26 +17,31 @@ public class StaticMethodsOverload
 	{
 		String Name= "Gibson";
 		printAll(Name);	//passed the mem address not the name
-		//16	17	 9	 11	  17   19 ; temp deleted
+		//18	19	 11	 13	  19 ; temp deleted
 		
 		
 		int myAge = 16;		//primative	gets sent rather that mem address
 		printAll(16);		//printAll String reject
 							//printAll int accept
-		//23	24	48	50	24	27
-		System.out.println("stop");
+		//23	24	53	55	24	 ; terminate
+		
+		byte myAgeB = (byte)18;					//Goes to the printAll short
+		printAll(myAgeB);	//implied cast
+							//printAll String reject
+							//PrintAll int implied cast byte to int
+		//28	29	57	59	29 ; terminate
+		//byte > short > int 	backwards compatable
+		//Long will not work	(compile error)
+		//long MyAgeL 23;
+		//printAll(MyAgeL);
+		
+		short myAgeS = (short) 17;
+		printAll(myAgeS);
+		//38	39	57	58	39 ; terminate
+		printAll(myAgeS, Name);
+		printAll(Name, myAgeS);
 	}
-	
-	
-	
-	
-	
-	
-	
 
-	
-	
-	
 //	Will not work	printAll + String  			< 			Illigal
 //	public static void printAll(String Address)
 //	{
@@ -47,7 +52,28 @@ public class StaticMethodsOverload
 	//Overloading
 	public static void printAll(int age)
 	{
-		System.out.println("age: " + age);
+		System.out.println("int age: " + age);
 	}
-}
+	public static void printAll(short age)
+	{
+		System.out.println("short age: " + age);
+	}
+	
+	
+	//1) exact match 
+	//2) closest match	(byte goes to short)
+	//3) 
+
+	//printAll short String
+	public static void printAll(short age, String Name)
+	{
+		System.out.println("short age: " + age + "\nString Name: " + Name);
+	}
+	
+	//printAll String short
+	public static void printAll(String Name, short age)
+	{
+		System.out.println("String Name: " + Name + "\nshort age: " + age);
+	}
+}	
 	
