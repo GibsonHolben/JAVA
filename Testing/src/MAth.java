72cb603431f81942b35b41ddb88c87dc2993f2f3
Mastermind <148354429+GibsonHolben@users.noreply.github.com>
2023-12-15 07:46:11 -0500

    metadata

---
+++ Testing/src/MAth.java
@@ -0,0 +1,19 @@
+
+/**
+ * @author CTEHolbenG46
+ * @version 0.0.1
+ */
+public class MAth
+{
+
+	public static void main(String[] args)
+	{
+		int y = 3;
+		int x = 2;
+		int z = 0;
+		z = --y + 3 + y-- * 6 + ++x + y;
+		System.out.println(z);
+		
+	}
+
+}
