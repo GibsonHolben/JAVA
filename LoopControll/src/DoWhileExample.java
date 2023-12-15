58cd45261f6aac568e301c7bb40aedd1011d6708
Mastermind <148354429+GibsonHolben@users.noreply.github.com>
2023-12-15 09:41:09 -0500

    Updated doWhile

--- LoopControll/src/DoWhileExample.java
+++ LoopControll/src/DoWhileExample.java
@@ -5,35 +5,53 @@ public class DoWhileExample
 
 	public static void main(String[] args)
 	{
-
-		//YN	N
+		
+		//Y	  S	  N
+		//Y	  N
 		//N
+		//B
 		
 		Scanner Input2 = new Scanner(System.in);
-		String answer = "";
+		
+		String answer2 = "";
 		
 		do
 		{
 			System.out.println("ok have fun");	
 			System.out.println("Would you like to continue Y/N ");
-			answer = Input2.nextLine();
-			answer = answer.toUpperCase();
+			answer2 = Input2.nextLine();
+			answer2 = answer2.toUpperCase();
 			
-		}while(answer.equals("Y"));
-		
-		System.out.println("ok bye");
+		}while(answer2.equals("Y") || answer2.equals("S"));	
 		
 		
-		System.out.println("****************************************************");
+//		String answer = "";
 		
-		int executeTimesStatic = 0;
-		
-		do
-		{
-			System.out.println("Keep going do While: " + executeTimesStatic);
-			executeTimesStatic--;
-			
-		}while(executeTimesStatic > 0);
+		//Y	  N
+		//N
+//		
+//		do
+//		{
+//			System.out.println("ok have fun");	
+//			System.out.println("Would you like to continue (second Loop Y/N ");
+//			answer = Input2.nextLine();
+//			answer = answer.toUpperCase();
+//			
+//		}while(answer.equals("Y"));	//dont forgt terminator
+//		
+//		System.out.println("ok bye");
+//		
+//		
+//		System.out.println("****************************************************");
+//		
+//		int executeTimesStatic = 0;
+//		
+//		do
+//		{
+//			System.out.println("Keep going do While: " + executeTimesStatic);
+//			executeTimesStatic--;
+//			
+//		}while(executeTimesStatic > 0);
 	
 
 		
