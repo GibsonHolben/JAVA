4e1cad38abac90ad23571fd7b5253ac12d39fae6
Mastermind <148354429+GibsonHolben@users.noreply.github.com>
2023-12-19 09:23:20 -0500

    mail updates

--- CertiportTests/src/Main.java
+++ CertiportTests/src/Main.java
@@ -6,13 +6,11 @@ public class Main
 	public static void main(String[] args) 
 	{
 		String s = "Gibson Holben";
-		
 		String s2 = s.substring(7, 13);
-		
 		ArrayList<String> users = new ArrayList<String>();
 		
-		print(s2);	//output = Holben
-		
+		//nestd if statments
+		print(s2);
 		if(s.length() > 6)
 		{
 			if(s.equals("Gibson Holben"))
@@ -21,23 +19,27 @@ public class Main
 			}
 		}
 		
+		//hex with String.format
 		int x = 1886;
 		print(String.format("%x", x)); 
 		
+		//array looping
 		print("How many users are you adding?");
 		Scanner scnr = new Scanner(System.in);
 		int loopTime = scnr.nextInt();
-		scnr.close();
 		
 		for(int i = 0; i < loopTime; i++)
 		{
-			print("Please enter the name");
-			Scanner scnr2 = new Scanner(System.in);
-			String name = scnr2.nextLine();
+			if(i != 0)
+				print("Please enter the name");
+			String name = scnr.nextLine();
 			users.add(name);
-			scnr2.close();
+			
+			print("");	
 		}
+		scnr.close();
 		
+		//array printing
 		for(int i = 0; i < users.size(); i++)
 		{
 			String ss = users.get(i);
@@ -45,6 +47,22 @@ public class Main
 		}
 		
 		
+		
+		
+		//++X 	 and	X++
+		int numA 	 = 5;
+		int numC 	 = 2;
+		int resaultA = 0;
+		
+		
+		resaultA = ++numA + numC - numA++;		//2
+			//		num10A + num10C - num10A++
+			//		 6	   +   2	- num10A++
+			//			   8	    -    6  ++
+			//			       		2	
+		print("ResaultA: " + Integer.toString(resaultA));
+		
+		print("stop");
 		
 	}
 	public static void print(String p)
