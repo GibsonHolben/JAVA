d44d74e085e08efa0f2ebe439f7ccb1533de72bb
Mastermind <148354429+GibsonHolben@users.noreply.github.com>
2023-12-19 09:40:03 -0500

    Added QestionTest.java
    Created a test file that is used for testing question of the java test

---
+++ CertiportTests/src/QuestionTest.java
@@ -0,0 +1,9 @@
+
+public class QuestionTest 
+{
+	public static void main(String[] args) 
+	{
+		String[] locations = {"North", "SOuth", "East", "West", null};
+		System.out.println(locations.length);
+	}
+}
