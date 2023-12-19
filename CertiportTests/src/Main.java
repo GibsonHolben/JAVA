b4aeb8b272ee07aa6aca1cdea1dcc8d97e6df5c7
Mastermind <148354429+GibsonHolben@users.noreply.github.com>
2023-12-19 08:44:22 -0500

    Testing + metadata

--- CertiportTests/src/Main.java
+++ CertiportTests/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main 
 {
@@ -6,6 +8,8 @@ public class Main
 		String s = "Gibson Holben";
 		
 		String s2 = s.substring(7, 13);
+		
+		ArrayList<String> users = new ArrayList<String>();
 		
 		print(s2);	//output = Holben
 		
@@ -19,10 +23,35 @@ public class Main
 		
 		int x = 1886;
 		print(String.format("%x", x)); 
+		
+		print("How many users are you adding?");
+		Scanner scnr = new Scanner(System.in);
+		int loopTime = scnr.nextInt();
+		scnr.close();
+		
+		for(int i = 0; i < loopTime; i++)
+		{
+			print("Please enter the name");
+			Scanner scnr2 = new Scanner(System.in);
+			String name = scnr2.nextLine();
+			users.add(name);
+			scnr2.close();
+		}
+		
+		for(int i = 0; i < users.size(); i++)
+		{
+			String ss = users.get(i);
+			print(ss);
+		}
+		
+		
+		
 	}
 	public static void print(String p)
 	{
 		System.out.println(p);
 	}
+	
+	
 	
 }
